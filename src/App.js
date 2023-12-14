d3f9806610bd65a99d37c5f73ce9f41e42d4aad3
Omkar <omkar.desai916@gmail.com>
2023-12-14 20:53:17 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import MainSection from "./section/main.js";
+import Nav from "./components/Nav.js";
 
 function App() {
-  return <div className="App">Hello test</div>;
+  return (
+    <div>
+      <Nav />
+      <MainSection />
+    </div>
+  );
 }
 
 export default App;
